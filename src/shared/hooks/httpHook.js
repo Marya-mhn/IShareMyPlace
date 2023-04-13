ab54b771c46930f39d4ad7d2cd06bbc1094f7598
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-04-13 02:56:49 +0100

    using the custom Http Hook

--- src/shared/hooks/httpHook.js
+++ src/shared/hooks/httpHook.js
@@ -1,20 +1,17 @@
-import {
-  useState,
-  useCallback,
-  useRef,
-  useEffect,
-} from "react/cjs/react.production.min";
+import { useState, useCallback, useRef, useEffect } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
   const activeHttpRequests = useRef([]);
+
   const sendRequest = useCallback(
     async (url, method = "GET", body = null, headers = {}) => {
       setIsLoading(true);
       const httpAbortCtrl = new AbortController();
       activeHttpRequests.current.push(httpAbortCtrl);
+
       try {
         const response = await fetch(url, {
           method,
@@ -24,15 +21,22 @@ export const useHttpClient = () => {
         });
 
         const responseData = await response.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
         if (!response.ok) {
           throw new Error(responseData.message);
         }
 
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
     },
     []
   );
@@ -43,8 +47,10 @@ export const useHttpClient = () => {
 
   useEffect(() => {
     return () => {
-      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abortCtrl());
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
     };
-  });
+  }, []);
+
   return { isLoading, error, sendRequest, clearError };
 };
