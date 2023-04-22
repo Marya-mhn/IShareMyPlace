051c18ac74689d31efbab99636117c663e2c21fa
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-04-22 23:01:35 +0100

    commit

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -10,6 +10,17 @@ const ImageUpload = (props) => {
 
   const filePickerRef = useRef();
 
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPreviewUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }, [file]);
+
   const pickedHandler = (event) => {
     let pickedFile;
     let fileIsValid = isValid;
@@ -41,12 +52,14 @@ const ImageUpload = (props) => {
       />
       <div className={`image-upload ${props.center && "center"}`}>
         <div className="image-upload__preview">
-          <img src="" alt="Preview" />
+          {previewUrl && <img src="" alt="Preview" />}
+          {!previewUrl && <p>Please pick an image.</p>}
         </div>
         <Button type="button" onClick={pickImageHandler}>
           PICK IMAGE
         </Button>
       </div>
+      {!isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
