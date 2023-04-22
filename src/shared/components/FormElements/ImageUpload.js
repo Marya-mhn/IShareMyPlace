1d8aa90b4bc4b49d52bcdc334c5647661ef76d2b
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-04-22 21:39:11 +0100

    commit

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -1,13 +1,28 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import Button from "./Button";
 import "./ImageUpload.css";
 
 const ImageUpload = (props) => {
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+  const [isValid, setIsValid] = useState(false);
+
   const filePickerRef = useRef();
 
   const pickedHandler = (event) => {
-    console.log(event.target);
+    let pickedFile;
+    let fileIsValid = isValid;
+    if (event.target.files && event.yarget.files.length === 1) {
+      pickedFile = event.target.files[0];
+      setFile(pickedFile);
+      setIsValid(true);
+      fileIsValid = true;
+    } else {
+      setIsValid(false);
+      fileIsValid = false;
+    }
+    props.onInput(props.id, pickedFile, fileIsValid);
   };
 
   const pickImageHandler = () => {
@@ -28,7 +43,7 @@ const ImageUpload = (props) => {
         <div className="image-upload__preview">
           <img src="" alt="Preview" />
         </div>
-        <Button type="button" onclick={pickImageHandler}>
+        <Button type="button" onClick={pickImageHandler}>
           PICK IMAGE
         </Button>
       </div>
