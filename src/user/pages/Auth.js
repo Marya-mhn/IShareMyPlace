96a5bf49abf827a84a50f95cb7687eaedb9b76ad
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-04-23 01:10:53 +0100

    fixing the path

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -90,7 +90,7 @@ const Auth = () => {
         formData.append("password", formState.inputs.password.value);
         formData.append("image", formState.inputs.image.value);
         const responseData = await sendRequest(
-          "http://localhost:5000/api/users/signup",
+          "http://localhost:9000/api/users/signup",
           "POST",
           formData
         );
